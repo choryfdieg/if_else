e01edbf0d8a16b83c3561d8a29268aa9aeb52496
Diego Garcia <diego.g.garcia@pwc.com>
2023-09-26 19:21:25 -0500

    he creado los primeros archivos de la pagina

---
+++ condicionales.js
@@ -0,0 +1,18 @@
+// camelCase
+// = asignacion
+// == comparacion
+let usuario = "juan";
+let permisosUsuario = "guardar, descargar, editar";
+
+let estaAutenticado = true;
+
+
+if( !estaAutenticado ){
+
+    alert("Debe primero estar autenticado");
+
+}else{
+
+    alert("Bienvenido a facebook");
+
+}
