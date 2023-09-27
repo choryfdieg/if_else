ad8000ce28ea8ac8ca6735d151a56a7be047a42c
JayderHerreraMora <hjaider003@gmail.com>
2023-09-26 19:31:46 -0500

    agregue una nueva condicion

--- condicionales.js
+++ condicionales.js
@@ -15,3 +15,17 @@ if( !estaAutenticado ){
     alert("Bienvenido a facebook");
 
 }
+
+let num1;
+let num2;
+
+num1 = 10;
+num2 = 20;
+
+if (num1 == num2) {
+    alert(num1 + "es igual a" + num2)
+} else if (num1 > num2) {
+    alert(num1 + " es mayor")
+}else{
+    alert(num2 + "es mayor")
+}
